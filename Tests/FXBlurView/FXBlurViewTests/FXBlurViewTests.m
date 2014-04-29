e628cf1fc09abaad30755ff322db1d9963bf4321
Ashton-W <Ashton-W@users.noreply.github.com>
2014-04-29 15:57:26 +1000

    Added Test project

---
+++ Tests/FXBlurView/FXBlurViewTests/FXBlurViewTests.m
@@ -0,0 +1,29 @@
+//
+//  FXBlurViewTests.m
+//  FXBlurViewTests
+//
+//  Created by Ashton Williams on 29/04/2014.
+//  Copyright (c) 2014 Charcoal Design. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FXBlurViewTests : XCTestCase
+
+@end
+
+@implementation FXBlurViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+@end
