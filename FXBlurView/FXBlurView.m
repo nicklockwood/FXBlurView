72ff36eca0499cd49f7b4d0ae5da836f2d0fbb28
Rich Hong <hong.rich@gmail.com>
2015-02-04 11:26:00 -0500

    Hide empty sublayers to prevent renderInContext crash on iOS 8

--- FXBlurView/FXBlurView.m
+++ FXBlurView/FXBlurView.m
@@ -606,6 +606,19 @@
     return snapshot;
 }
 
+- (NSArray *)hideEmptyLayers:(CALayer *)layer
+{
+    NSMutableArray *layers = [NSMutableArray array];
+    if (CGRectIsEmpty(layer.bounds)) {
+        layer.hidden = YES;
+        [layers addObject:layer];
+    }
+    for (CALayer *sublayer in layer.sublayers) {
+        [layers addObjectsFromArray:[self hideEmptyLayers:sublayer]];
+    }
+    return layers;
+}
+
 - (NSArray *)prepareUnderlyingViewForSnapshot
 {
     __strong CALayer *blurlayer = [self blurLayer];
@@ -628,6 +641,10 @@
             }
         }
     }
+
+    // Also hide any sublayers with empty bounds to prevent a crash on iOS 8
+    [layers addObjectsFromArray:[self hideEmptyLayers:underlyingLayer]];
+
     return layers;
 }
 
